8df7d4bbcb6b654c78743a352ac32e10b1755c7c
Sebastian Aigner <sebastian.aigner@tum.de>
2018-09-05 19:48:13 +0200

    Increase Limit for all time posts

--- src/GameMode.kt
+++ src/GameMode.kt
@@ -3,7 +3,7 @@ package io.sebi
 import net.dean.jraw.models.TimePeriod
 
 enum class GameMode(val subreddit: String, var timeLimit: TimePeriod, var limit: Int) {
-    TOP_ALL_TIME("disneyVacation", TimePeriod.ALL, 500),
+    TOP_ALL_TIME("disneyVacation", TimePeriod.ALL, 800),
     TOP_THIS_MONTH("disneyVacation", TimePeriod.MONTH, 300),
     TOP_THIS_WEEK("disneyVacation", TimePeriod.WEEK, 100),
     HOT("disneyVacation", TimePeriod.DAY,50),
